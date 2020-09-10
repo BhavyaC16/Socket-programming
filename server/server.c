11b0fdc9146d558e1580de3fc38b330f9f7e1771
Bhavya Chopra <bhavya18333@iiitd.ac.in>
2020-09-10 20:30:28 +0530

    Update server.c

--- server/server.c
+++ server/server.c
@@ -23,6 +23,7 @@ int main()
 	server.sin_port = htons(PORT);
 	server.sin_addr.s_addr = INADDR_ANY;
 
+	//server creates socket
 	socketID = socket(AF_INET, SOCK_STREAM, 0);
 	if(socketID==-1){
 		perror("Error in socket creation");
@@ -30,9 +31,13 @@ int main()
 	}
 	printf("Socket created\n");
 
+	//server ensures that the port is available and allocates
+	//buffer and controlled time-outs.
 	int opt = 1;
 	setsockopt(socketID, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
 
+
+	//server bind the socket
 	int bind_status = bind(socketID, (struct sockaddr *)&server, sizeof(server));
 	if(bind_status!=0){
 		perror("Error in socket binding");
@@ -40,6 +45,7 @@ int main()
 	}
 	printf("Socket binded\n");
 
+	//the server listens for clients trying to connect to it
 	int listening_status = listen(socketID, 0);
 	if(listening_status!=0)
 	{
@@ -47,8 +53,12 @@ int main()
 		exit(1);
 	}
 	printf("Server is ready\n");
+
+	//The server stays active after file transfers.
+	//Only one client can connect to the server at a time.
 	while(1)
 	{
+		//server accepts client connection
 		client = accept(socketID, (struct sockaddr *)&server, (socklen_t *)&address_length);
 		if(client<0){
 			perror("Client connection failed");
@@ -58,6 +68,7 @@ int main()
 
 		FILE* fp;
 
+		//server receives filename from the client
 		int correct_filename = 0;
 		while(correct_filename==0){
 			char filenameRecd[1024] = {0};
@@ -69,6 +80,8 @@ int main()
 			printf("Filename sent by client: %s\n", filenameRecd);
 			printf("Path to file: %s\n", file_path);
 
+			// PART-D: SERVER SEARCHES FOR FILE IN SHARED DRIVE
+			//         AND RETURNS THE STATUS TO CLIENT
 			char file_found_status[2] = {0};
 			fp = fopen(file_path, "rb");
 			if(fp==NULL){
@@ -84,8 +97,8 @@ int main()
 		}
 
 		printf("File opened\nStarting file transfer\n");
-		char success_status[2] = {0};
-		strcpy(success_status, "1");
+
+		// PART-E: SERVER SEND THE FILE TO THE CLIENT 
 		int x;
 		char content[1025] = {0};
 		x = fread(content, 1, MAX_BUFF, fp);
@@ -99,23 +112,15 @@ int main()
 			memset(content, 0, MAX_BUFF);
 			x = fread(content, 1, MAX_BUFF, fp);
 		}
-
+		printf("File transferred\nClosing the file\n");
 		fclose(fp);
 
-		//char client_ack[2] = {0};
-		//read(client, client_ack, 2);
-		//printf("%s\n", client_ack);
-		//send(client, success_status, 2, 0);
-		//printf("send status\n");
-		//if(strcmp(success_status, "1")==0){
-		//	printf("File sent to client\nClosing connection");
-		//}
-		//else{
-		//	printf("Encountered error while sending file\nClosing connection\n");
-		//}
+		//Server closes connection to client after file transfer
+		printf("Closing connection to client");
 		close(client);
 	}
 
+	//Closing the server
 	printf("Closing server\n");
 	close(socketID);
 }
